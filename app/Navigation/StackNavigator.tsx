45c906e0fa3d5d1b93a44a6db835929d93e13d8e
Obafemi <dadaobafemi26@gmail.com>
2025-07-28 21:30:33 +0100

    renamed components

--- app/Navigation/StackNavigator.tsx
+++ app/Navigation/StackNavigator.tsx
@@ -1,17 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Viewone from "../Onboarding/Viewone";
+import View3 from "../Onboarding/Viewthree";
+import View2 from "../Onboarding/Viewtwo";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
+    <Stack.Navigator
+      initialRouteName="View1"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="View1" component={Viewone} />
+      <Stack.Screen name="View2" component={View2} />
+      <Stack.Screen name="View3" component={View3} />
+    </Stack.Navigator>
   );
 };
 
 export default StackNavigator;
-
-const styles = StyleSheet.create({});
---
+++ app/Onboarding/Viewone.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+// import ViewImage from "../../assets/images/View1.svg";
+
+const Viewone = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={styles.loader}>
+        <Text>Hello</Text>
+      </View>
+      <View style={{ paddingHorizontal: 25 }}>
+        <View>
+          <Text
+            style={{
+              fontSize: 40,
+              fontWeight: 500,
+              color: "#333333",
+              lineHeight: 48,
+              letterSpacing: 0,
+              maxWidth: "80%",
+            }}
+          >
+            Save better with smart Vaults.
+          </Text>
+          <Text
+            style={{
+              fontSize: 16,
+              marginTop: 20,
+              lineHeight: 24,
+              maxWidth: "80%",
+            }}
+          >
+            Personalized saving vaults for your goals — rent, travel, gadgets,
+            and more+
+          </Text>
+        </View>
+        <View>{/* <ViewImage /> */}</View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Viewone;
+
+const styles = StyleSheet.create({
+  loader: {
+    height: 30,
+    backgroundColor: "blue",
+    paddingHorizontal: 30,
+  },
+});
